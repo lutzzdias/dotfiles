c7783a1d8373c39593df0b01ec11e3fa3fd9d15d
lutzzdias <thiagolutzdias@hotmail.com>
2024-01-29 12:44:54 +0000

    ts-textobjects: improve keymaps descriptions

--- nvim/lua/plugins/ts-textobjects.lua
+++ nvim/lua/plugins/ts-textobjects.lua
@@ -18,10 +18,10 @@ return {
 					lookahead = true,
 
 					keymaps = {
-						["af"] = { query = "@function.outer", desc = "Select outer part of a function" },
-						["if"] = { query = "@function.inner", desc = "Select inner part of a function" },
-						["ac"] = { query = "@class.outer", desc = "Select outer part of a class" },
-						["ic"] = { query = "@class.inner", desc = "Select inner part of a class" },
+						["af"] = { query = "@function.outer", desc = "Select function" },
+						["if"] = { query = "@function.inner", desc = "Select within function" },
+						["ac"] = { query = "@class.outer", desc = "Select class" },
+						["ic"] = { query = "@class.inner", desc = "Select within class" },
 						["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
 					},
 
