db2629c48e81808329d40aa40c7bed1cbab03347
lutzzdias <thiagolutzdias@hotmail.com>
2024-01-29 14:23:59 +0000

    nvim/lazygit: config for lazygit plugin

--- nvim/lua/plugins/lazygit.lua
+++ nvim/lua/plugins/lazygit.lua
@@ -2,10 +2,26 @@ return {
 	"kdheepak/lazygit.nvim",
 	event = "VeryLazy",
 	dependencies = {
+		"nvim-telescope/telescope.nvim",
 		"nvim-lua/plenary.nvim",
 	},
 	config = function()
 		-- import
-		local lazygit = require("lazygit")
+		local telescope = require("telescope")
+		local lazy_utils = require("lazygit.utils")
+
+		telescope.load_extension("lazygit")
+
+		-- keymaps
+		local opts = { noremap = true, silent = true }
+
+		opts.desc = "Open lazygit"
+		vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<CR>", opts)
+
+		opts.desc = "Open current git repos with telescope"
+		vim.keymap.set("n", "<leader>gr", function()
+			lazy_utils.project_root_dir()
+			telescope.extensions.lazygit.lazygit()
+		end, opts)
 	end,
 }
