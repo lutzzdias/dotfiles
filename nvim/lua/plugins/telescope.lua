67975926e496e53d305620963a23acdace0a46b8
lutzzdias <thiagolutzdias@hotmail.com>
2024-01-29 14:31:08 +0000

    telescope: add opts to keymaps + buffers keymap

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -28,10 +28,21 @@ return {
 
 		-- keymaps
 		local keymap = vim.keymap -- for conciseness
+		local opts = { noremap = true, silent = true }
 
-		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+		opts.desc = "Fuzzy find files in cwd"
+		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", opts)
+
+		opts.desc = "Fuzzy find recent files"
+		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", opts)
+
+		opts.desc = "Fuzzy find open buffers"
+		keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", opts)
+
+		opts.desc = "Find string in cwd"
+		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", opts)
+
+		opts.desc = "Find string under cursor in cwd"
+		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", opts)
 	end,
 }
