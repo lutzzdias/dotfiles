d6a81493349828398b119d0ca6609afce7d7032d
lutzzdias <thiagolutzdias@hotmail.com>
2024-01-29 14:30:34 +0000

    devicons: remove unnecessary configs

--- nvim/lua/plugins/web-devicons.lua
+++ nvim/lua/plugins/web-devicons.lua
@@ -1,12 +1,4 @@
 return {
 	"nvim-tree/nvim-web-devicons",
-	config = function()
-		-- import
-		local devicons = require("nvim-web-devicons")
-
-		devicons.setup({
-			override = {},
-			default = true,
-		})
-	end,
+	event = "VeryLazy",
 }
