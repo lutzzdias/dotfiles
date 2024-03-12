a906ee47a64cac2f1085f52c40e0435c9634a6b0
lutzzdias <thiagolutzdias@hotmail.com>
2024-03-12 10:28:46 +0000

    cmp: refactor and delay
    still needs some styling improvements

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -2,10 +2,34 @@ return {
 	"hrsh7th/nvim-cmp",
 	event = "InsertEnter",
 	dependencies = {
-		"L3MON4D3/LuaSnip", -- snippet engine
-		"saadparwaiz1/cmp_luasnip", -- for autocompletion
+		-- snippet engine
+		{
+			"L3MON4D3/LuaSnip",
+			dependencies = "rafamadriz/friendly-snippets", -- useful snippets
+			config = function()
+				local luasnip = require("luasnip")
 
-		"rafamadriz/friendly-snippets", -- useful snippets
+				-- load snippets
+				require("luasnip.loaders.from_vscode").lazy_load()
+				require("luasnip.loaders.from_snipmate").lazy_load()
+				require("luasnip.loaders.from_lua").lazy_load()
+
+				-- setup
+				luasnip.setup({
+					history = true,
+					updateevents = "TextChanged,TextChangedI",
+				})
+			end,
+		},
+
+		-- completion sources
+		{
+			"saadparwaiz1/cmp_luasnip",
+			"hrsh7th/cmp-nvim-lua",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+		},
 	},
 
 	config = function()
@@ -13,12 +37,52 @@ return {
 		local cmp = require("cmp")
 		local luasnip = require("luasnip")
 
-		require("luasnip.loaders.from_vscode").lazy_load()
-
+		-- TODO: make wait before showing the completion menu
 		cmp.setup({
 			completion = {
-				completeopt = "menu,menuone,preview,noselect",
+				completeopt = "menu,menuone",
+				autocomplete = false,
 			},
+
+			sources = {
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
+				{ name = "nvim_lua" },
+				{ name = "buffer" },
+				{ name = "path" },
+			},
+
+			window = {
+				completion = {
+					side_padding = 1,
+					-- TODO: make pretty
+					winhighlight = "Normal:Pmenu,CursorLine:PmenuSel,Search:None",
+					scrollbar = false,
+				},
+				documentation = {
+					border = {
+						{ "╭", "CmpDocumentationBorder" },
+						{ "─", "CmpDocumentationBorder" },
+						{ "╮", "CmpDocumentationBorder" },
+						{ "│", "CmpDocumentationBorder" },
+						{ "╯", "CmpDocumentationBorder" },
+						{ "─", "CmpDocumentationBorder" },
+						{ "╰", "CmpDocumentationBorder" },
+						{ "│", "CmpDocumentationBorder" },
+					},
+					winhighlight = "Normal:CmpDocumentation",
+				},
+			},
+
+			-- TODO: add icons
+			-- formatting = {
+			-- 	fields = { "abbr", "kind", "menu" },
+			-- 	format = function(_, item)
+			-- 		local icon = " " .. item.kind .. " "
+			-- 		item.kind = string.format("%s %s", icon, item.kind)
+			-- 		return item
+			-- 	end,
+			-- },
 
 			snippet = { -- configure how nvim-cmp interacts with snippet engine
 				expand = function(args)
@@ -26,22 +90,38 @@ return {
 				end,
 			},
 
+			-- TODO: these definitions are not working
 			-- keymaps
-			mapping = cmp.mapping.preset.insert({
+			mapping = {
 				["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
 				["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
 				["<C-U>"] = cmp.mapping.scroll_docs(-4),
 				["<C-D>"] = cmp.mapping.scroll_docs(4),
-				["<C-s>"] = cmp.mapping.complete(), -- show completion suggestions
-				["<C-e>"] = cmp.mapping.abort(), -- close completion window
+				["<C-space>"] = cmp.mapping.complete(), -- show completion suggestions
+				["<C-e>"] = cmp.mapping.close(), -- close completion window
 				["<CR>"] = cmp.mapping.confirm({ select = false }),
-			}),
+			},
+		})
 
-			-- sources for autocompletion
-			sources = cmp.config.sources({
-				{ name = "nvim_lsp" }, -- lsp
-				{ name = "luasnip" }, -- snippets
-			}),
+		-- Setup delayed completion
+		local timer = nil
+		vim.api.nvim_create_autocmd({ "TextChangedI", "CmdlineChanged" }, {
+			pattern = "*",
+			callback = function()
+				if timer then
+					vim.loop.timer_stop(timer)
+					timer = nil
+				end
+
+				timer = vim.loop.new_timer()
+				timer:start(
+					200,
+					0,
+					vim.schedule_wrap(function()
+						cmp.complete({ reason = cmp.ContextReason.Auto })
+					end)
+				)
+			end,
 		})
 	end,
 }
