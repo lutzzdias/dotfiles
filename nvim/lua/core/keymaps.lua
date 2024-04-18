d06b009847a487ed4215766843c686b2056c2986
lutzzdias <thiagolutzdias@hotmail.com>
2024-04-18 05:01:25 +0100

    nvim: add opts with desc and remove unnecessary comments

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -3,37 +3,57 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = ","
 
 local keymap = vim.keymap -- for conciseness
+local opts = { noremap = true, silent = true }
 
 ------------------- General Keymaps -------------------
-
--- use jk to exit insert mode
-keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
-
--- clear search highlights
-keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
 -- delete single character without copying into register
 keymap.set("n", "x", '"_x')
 
--- increment/decrement numbers
-keymap.set("n", "+", "<C-a>", { desc = "Increment number" }) -- increment
-keymap.set("n", "-", "<C-x>", { desc = "Decrement number" }) -- decrement
+opts.desc = "Exit insert mode with jk"
+keymap.set("i", "jk", "<ESC>", opts)
+
+opts.desc = "Clear search highlights"
+keymap.set("n", "<leader>nh", ":nohl<CR>", opts)
+
+opts.desc = "Increment number"
+keymap.set("n", "+", "<C-a>", opts)
+
+opts.desc = "Decrement number"
+keymap.set("n", "-", "<C-x>", opts)
 
 ------------------- Window Management Keymaps -------------------
 
--- window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+opts.desc = "Split window vertically"
+keymap.set("n", "<leader>sv", "<C-w>v", opts)
+
+opts.desc = "Split window horizontally"
+keymap.set("n", "<leader>sh", "<C-w>s", opts)
+
+opts.desc = "Make splits equal size"
+keymap.set("n", "<leader>se", "<C-w>=", opts)
+
+opts.desc = "Close current split"
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", opts)
 
 -- tab management
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
+opts.desc = "Create new tab"
+keymap.set("n", "<leader>tc", "<cmd>tabnew<CR>", opts)
+
+opts.desc = "Close current tab"
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", opts)
+
+opts.desc = "Jump to tab"
 keymap.set("n", "<leader>t", function()
 	local tabnum = vim.fn.nr2char(vim.fn.getchar())
 	vim.cmd("tabn" .. tabnum)
-end, { desc = "Jump to tab" }) -- jump to specified tab
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+end, opts)
+
+opts.desc = "Go to next tab"
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", opts)
+
+opts.desc = "Go to previous tab"
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", opts)
+
+opts.desc = "Open current buffer in new tab"
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", opts)
