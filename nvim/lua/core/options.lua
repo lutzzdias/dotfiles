abfb8290483b2ff1ce8ee219eb393375bee78cc0
lutzzdias <thiagolutzdias@hotmail.com>
2024-04-18 05:02:28 +0100

    nvim: remove redundant configs with defaults

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -16,7 +16,7 @@ opt.numberwidth = 1 -- show current and total line number in 'ruler'
 opt.tabstop = 4 -- 4 spaces for tabs (prettier default)
 opt.shiftwidth = 4 -- 4 spaces for indent width
 opt.expandtab = true -- expand tab to spaces
-opt.autoindent = true -- copy indent from current line when starting new one
+opt.smartindent = true -- copy indent from current line when starting new one
 
 -- line wrapping
 opt.wrap = false -- disable line wrapping
@@ -24,22 +24,13 @@ opt.wrap = false -- disable line wrapping
 -- search settings
 opt.ignorecase = true -- ignore case when searching
 opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
-opt.incsearch = true -- show search matches as you type
-
--- cursor line
-opt.cursorline = true -- highlight the current cursor line
 
 -- appearance
 opt.termguicolors = true -- turn on termguicolors
-opt.background = "dark" -- colorschemes that can be light or dark will be made dark
 opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 opt.showtabline = 0 -- hide default tabline
-opt.showcmd = true -- show command in bottom bar
 opt.cursorline = true -- highlight current line
 opt.colorcolumn = "80" -- highlight 80th column
-
--- backspace
-opt.backspace = { "indent", "eol", "start" } -- allow backspace on indent, end of line or insert mode start position
 
 -- clipboard
 opt.clipboard:append("unnamedplus") -- use system clipboard as default register
